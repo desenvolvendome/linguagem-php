5571dff3c8f91d4db464caa8024c48bc5359f7d7
repl.it user <replituser@example.com>
2020-10-03 00:36:33 +0000

    Criando diretório e implementando a classe bubblesort

---
+++ mauricio-alves/linguagem-php/bubblesort.php
@@ -0,0 +1,17 @@
+<?php
+
+function bubbleSort($array)
+{
+  for($i = 0; $i < count($array); $i++)
+  {
+     for($j = 0; $j < count($array) - 1; $j++)
+     {
+       if($array[$j] > $array[$j + 1])
+       {
+         $aux = $array[$j];
+         $array[$j] = $array[$j + 1];
+         $array[$j + 1] = $aux;
+       }
+     }
+  }
+}
