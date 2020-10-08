210b1e2e93a1676e85d9d28e2e1bc397d16fb582
repl.it user <replituser@example.com>
2020-10-08 19:50:30 +0000

    Added factorial and finish excirse

--- arlisson-jaques/php/main.php
+++ arlisson-jaques/php/main.php
@@ -2,11 +2,11 @@
 
 require_once("algorithms/bubblesort.php");
 require_once("algorithms/linearsearch.php");
-
-
+require_once("algorithms/factorial.php");
 
 
 bubblesort();
+printf("\nFactorial: %d", factorial(2));
 printf("Position: %d", linearSearch(7));
 
 ?>
